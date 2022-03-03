ba457fbee83e4c23f3fc75f894154f5f8bb6764d
Rahul Umap <umaprahul26@gmail.com>
2022-03-03 14:32:59 +0530

    Added tabbar

--- Instagram/Instagram/ContentView.swift
+++ Instagram/Instagram/ContentView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            
+            Text("Reels")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "video.circle.fill")
+                    Text("Reels")
+                }
+        }
+        .accentColor(.red)
+        .onAppear() {
+            UITabBar.appearance().barTintColor = .white
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
