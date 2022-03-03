f6cb33f0021a8d057f709cf62c65c257b26b9fe8
Rahul Umap <umaprahul26@gmail.com>
2022-03-03 14:22:41 +0530

    Initial commit

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Rahul on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
